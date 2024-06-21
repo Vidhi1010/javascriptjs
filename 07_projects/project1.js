02af5d8f00e98b967b2101932d71e76270003540
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-06-21 23:40:20 +0530

    addition of projects

---
+++ 07_projects/project1.js
@@ -0,0 +1,46 @@
+/* const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach ( function (button) {
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+    });
+}); */
+
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach ( function (button) {
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target);
+        const color = e.target.id;
+        switch(color){
+            case 'grey' : body.style.backgroundColor = e.target.id;
+            break;
+            case 'white' : body.style.backgroundColor = e.target.id;
+            break;
+            case 'blue' : body.style.backgroundColor = e.target.id;
+            break;
+            case 'yellow' : body.style.backgroundColor = e.target.id;
+            break;
+            default: console.log('Unknown color:', color);
+        }
+            
+    });
+});
