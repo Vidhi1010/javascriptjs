8721939b096a4e62ef331bf487770ab21e25d32a
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-06-26 14:11:51 +0530

    project added

---
+++ 07_projects/guessTheNumber.js
@@ -0,0 +1,7 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.querySelector('#subt');
+const guessSlot = document.querySelector('.guesses');
+const userInput = document.querySelector('#guessField');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
