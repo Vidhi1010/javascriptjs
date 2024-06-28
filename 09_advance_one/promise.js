868bd83171c117c957bd4d47bf0d1769a670e0ae
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-06-28 21:45:25 +0530

    Promises

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -1,1 +1,23 @@
-console.log("promises");
+//The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+const promiseOne = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+
+        console.log('Async task is complete');
+        //resolve();
+
+    },1000);
+});
+
+ promiseOne.then(function(){
+    console.log("Promise consumed");
+ })
+
+ new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task2 is completed");
+        resolve();
+    }, 1000);
+ }).then(function(){
+    console.log("Async 2 resolve");
+ })
