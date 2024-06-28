ea63a5e6fbb2a47aacd80dcca0e07f5655278c16
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-06-28 22:19:49 +0530

    promises

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -21,3 +21,36 @@ const promiseOne = new Promise(function(resolve, reject) {
  }).then(function(){
     console.log("Async 2 resolve");
  })
+
+ const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username : "vidhi", email : "vidhi@example.com"});
+    }, 1000)
+ });
+
+ promiseThree.then(function(user){
+    console.log(user);
+ })
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error) {
+            resolve({username : "vidhi", passward : "123"});
+        }else{
+            reject('ERROR: Something went wrong');
+        }
+    }, 1000);
+});
+
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username;
+})
+.then((username) => {
+    console.log(username);
+})
+.catch(function(error) {
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"));
