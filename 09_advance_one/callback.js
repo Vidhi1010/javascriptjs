a72782e5e6c88c5b86d10ad0919d5e10ca0bb45d
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-06-30 14:12:07 +0530

    Callback Hell

--- 09_advance_one/callback.js
+++ 09_advance_one/callback.js
@@ -1,8 +1,34 @@
-    function sum(a, b) {
-        console.log(a + b);
-    }
+function sum(a, b) {
+  console.log(a + b);
+}
 
-    function calculator(a, b, sumCallback) {
-        sumCallback(a, b);
+function calculator(a, b, sumCallback) {
+  sumCallback(a, b);
+}
+calculator(1, 2, sum);
+
+//Callback hell: Nested callbacks stacked below one another forming a pyramid structure.(Pyramid of Doom)
+
+/* let age = 19;
+if(age >= 18) {
+    if(age>= 60) {
+        console.log("middle");
+    } else {
+        console.log("middle");
     }
-    calculator(1, 2, sum);
+}else {
+    console.log("child");
+} */
+
+function getData(dataId, getNextData) {
+  setTimeout(() => {
+    console.log("data", dataId);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 2000);
+}
+
+getData(1, () => {
+  getData(2);
+});
