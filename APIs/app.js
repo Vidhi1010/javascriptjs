32edd05d8d67abdc44058802d813500f8cd66fd2
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-07-25 23:32:24 +0530

    callback

--- APIs/app.js
+++ APIs/app.js
@@ -3,3 +3,17 @@
 /* let promise = fetch(URL);
 console.log(promise); */
 
+/* setTimeout(() => {
+    console.log("hello")
+}, 2000);  */ 
+
+//callback- don't pass function with paranthesis as an argument 
+
+function sum (a, b) {
+    console.log(a + b);
+}
+function calculator(a, b , sumCallback) {
+    sumCallback(a, b);
+}
+
+calculator(1, 2, sum);
