ab0d2a97813940dd59d07bb7f059a5e7bca02911
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-07-27 02:40:30 +0530

    updations

--- 09_advance_one/asyncawait.js
+++ 09_advance_one/asyncawait.js
@@ -1,7 +1,7 @@
 //async function always returns a promise
 //await pauses the execution of its surrounding async function until the promise is settled
 
-function api() {
+/* function api() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log("weather data");
@@ -12,4 +12,24 @@ function api() {
 
 async function getWeatherData() {
     await api();
+} */
+
+function getData(dataId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+      resolve(200);
+    }, 2000);
+  });
+}
+
+async function getAllData() {
+    console.log("getting data1...");
+  await getData(1);
+  console.log("getting data2...");
+  await getData(2);
+  console.log("getting data3...");
+  await getData(3);
+  console.log("getting data4...");
+  await getData(4);
 }
