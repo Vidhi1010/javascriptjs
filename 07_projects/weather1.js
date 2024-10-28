6ea4341aaaf96866d8371c61b8ae503abd14148e
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-10-29 00:04:56 +0530

    weather app

--- 07_projects/weather1.js
+++ 07_projects/weather1.js
@@ -1,5 +1,8 @@
 const button = document.getElementById('search-btn');
 const input = document.getElementById('city-input');
+const cityName = document.getElementById('city-name');
+const cityTime = document.getElementById('city-time');
+const cityTemp = document.getElementById('city-temp');
 
 async function getData(cityName){
    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=d90a7686af3b40c19a4175112242510&q=${cityName}&aqi=yes`);
@@ -9,6 +12,10 @@ async function getData(cityName){
 button.addEventListener('click', async ()=> {
     const value = input.value;
     const result = await getData(value);
+    cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
     console.log(result);
+    cityTime.innerText = result.location.localtime;
+    cityTemp.innerText = `${result.current.temp_c} °C `;
 });
 
+ 
