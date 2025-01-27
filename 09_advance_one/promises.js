36f1fde47c29ef24e1a65ea5ad6b3c1ca65e7592
Vidhi1010 <vidhisinghvp1010@gmail.com>
2025-01-27 22:55:18 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -5,7 +5,7 @@ const promiseOne = new Promise(function(resolve, reject){
         console.log('Async task is compelete');
         resolve()
     }, 1000)
-})
+}) 
 
 promiseOne.then(function(){
     console.log("Promise consumed");
--- Revision/promise.js
+++ Revision/promise.js
@@ -23,3 +23,56 @@ p1.then((d) => {
 //The .then() method is designed to work with any resolved value (not just promises). It's flexible enough to handle both asynchronous operations (by chaining promises) and synchronous values (like strings, numbers, or objects).
 
 //If you return a string (or any value), it behaves as if that value was resolved immediately, and .then() simply forwards it to the next .then() in the chain
+
+const promiseOne = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    //console.log('Async task is complete');
+    resolve();
+  }, 1000);
+});
+
+promiseOne.then(function () {
+  //console.log("promise consumed");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    //console.log("Async task2")
+    resolve();
+  }, 1000);
+}).then(function () {
+  //console.log("Async 2 resolved");
+});
+
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Vidhi", email: "vidhi@example.com" });
+  }, 1000);
+});
+promiseThree.then(function (user) {
+  //console.log(user);
+});
+
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve({ username: "Vidhi", passward: "123" });
+    } else {
+      reject("ERROR: Something went wrong");
+    }
+  });
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  }).finally(() => console.log("The promise is either resolved or rejected"));
